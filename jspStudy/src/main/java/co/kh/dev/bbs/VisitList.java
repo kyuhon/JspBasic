5afe2d532a415a54a5d375ca4725fd30c9f41a4d
DESKTOP-9FU9MPH\정건일 <kyuhon123@gmail.com>
2024-12-09 02:50:00 +0900

    [modify] 브렌치 home_pc생성

--- jspStudy/src/main/java/co/kh/dev/bbs/VisitList.java
+++ jspStudy/src/main/java/co/kh/dev/bbs/VisitList.java
@@ -13,10 +13,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class VisitList
- */
-//@WebServlet(name = "visitList", urlPatterns = { "/visitList" })
 public class VisitList extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
